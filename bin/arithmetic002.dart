2b8342cd3292be469f5f6e14a2ac24eb198069c5
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-21 19:08:00 +0500

    bin/arithmetic002.dart

--- bin/arithmetic002.dart
+++ bin/arithmetic002.dart
@@ -1,4 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+int func(int a) {
+  int b;
+  b = a % 10;
+  a ~/= 10;
+  return a - b;
+}
 
-void main() {}
+void main() {
+  print(func(457));
+}
