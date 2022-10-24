db83299d7802f2ca6191e3e679cb1913be952962
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-24 20:28:35 +0500

    arithmetic040.dart

--- bin/arithmetic040.dart
+++ bin/arithmetic040.dart
@@ -2,10 +2,10 @@
 // the task condition is in the Readme file
 import 'dart:math';
 
-num func(int a, int b) {
+num func(int a) {
   return (a * (a + 1)) / pow(2, a);
 }
 
 void main() {
-  print(func(2, 6));
+  print(func(4));
 }
